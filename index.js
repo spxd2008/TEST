8e782fd989edb7d87df780c3b26ceeeb901c43f8
spxd2008 <gitrender76@gmail.com>
2026-01-05 16:28:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94718461889']
+const ownerNumber = ['94742349884']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
